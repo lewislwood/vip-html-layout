e6bb5596c44a550334ea6bf286be34ac7deaf6b6
Lewis Wood <lewislwood@gmail.com>
2024-05-11 08:30:34 -0500

    vite config

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+/** @type {import('vite').UserConfig} */
+ // import { defineConfig } from 'vite'
+export default {
+  build: {
+    outDir: "docs"
+  },
+  base: "https://lewislwood.github.io/vip_html_layout"
+}
